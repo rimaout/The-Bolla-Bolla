0f0de2051346a30c4a954852f94785eb1d8ce4cb
Jaxkeeper <116072651+Jaxkeeper@users.noreply.github.com>
2024-09-18 17:18:40 +0200

    BugFix: Player couldn't shoot bubbles near the roof

--- src/utilz/HelpMethods.java
+++ src/utilz/HelpMethods.java
@@ -8,7 +8,7 @@ import java.awt.geom.Rectangle2D;
 public class HelpMethods {
 
     public static boolean IsTileRoof(int yTile) {
-        return yTile < 5;
+        return yTile < 4;
     }
 
     public static boolean IsWall(int xTile, int yTile, int[][] levelData) {
